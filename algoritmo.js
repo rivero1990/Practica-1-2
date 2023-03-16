1666c207ab16aba9eafe3c3765bcbed0494bcd67
Anibal Rivero <riveroleonel@gmail.com>
2023-03-16 20:32:26 -0300

    segundo commit

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+document.querySelector("body").innerHTML = `
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hOQehj6776g" title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <p>video de luisito comunica en la india</p>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0111254485073!2d-58.413183749274125!3d-34.60388018036357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca8c854bd13f%3A0xfc0cf5c054da4aa1!2sAbasto%20Shopping!5e0!3m2!1ses-419!2sar!4v1679004052290!5m2!1ses-419!2sar"
+        width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <p>ubicacion del shopping abasto</p>
+
+    <iframe style="border-radius:12px"
+        src="https://open.spotify.com/embed/track/2SiXAy7TuUkycRVbbWDEpo?utm_source=generator" width="100%" height="352"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <p>musica de ac dc</p>
+
+    <iframe height="500px" width="1000px" src="https://www.adidas.com.ar/https://www.adidas.com.ar/river_plateriver_plate" frameborder="0"></iframe>
+`
